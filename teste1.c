cdc2448b30f5b84f8e7f9d34ff72e43468690850
allerman <allerman3301@gmail.com>
2023-03-09 00:06:47 -0300

    calculos com as variaveis

---
+++ teste1.c
@@ -0,0 +1,19 @@
+//Programa realiza calculos com as variaveis.
+//por Thayssa A. Silva 2023
+
+# include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+int INDICE = 13, SOMA = 0 , K = 0;
+
+    while(K < INDICE){
+        K = K + 1;
+        SOMA = SOMA + K;
+
+    }
+
+    printf("%i",SOMA);
+}
+
+//Resposta = 91
